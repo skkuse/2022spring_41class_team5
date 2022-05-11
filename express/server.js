a10a5f0061361775cffcd00f72cb9bb36f46e85d
CEOJINSUNG <jinsung1048@gmail.com>
2022-05-11 14:42:20 +0900

    [진성] socket.io 환경 설정

--- express/server.js
+++ express/server.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const http = require('http')
 
+// 기본적인 환경 세팅
 app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
@@ -10,7 +12,35 @@ app.use(bodyParser.json())
 const code = require("./api/code")
 app.use('/api/code', code)
 
+const server = http.createServer(app)
 const port = process.env.PORT || 8000
-app.listen(port, () => {
+
+// Socket.io를 app.io에 세팅
+app.io = require('socket.io')(server, {
+    cors: {
+        origin: "*",
+        credentials: true,
+    }
+})
+
+// socket은 기본적으로 event 방식으로 작동
+app.io.console('connection', (socket) => {
+    console.log('User In')
+
+    socket.on('disconnect', () => {
+        console.log('User Out')
+    })
+
+    // chat-msg라는 이벤트가 msg라는 object 형태로 들어오면 
+    // 서버에서 emit() 함수를 통하여 'chat-msg'라는 이벤트로 동일한 내용을 
+    // 전체 사용자에게 뿌려줍니다. 
+    socket.on('chat-msg', (msg) => {
+        app.io.emit('chat-msg', msg)
+    })
+})
+
+// app.io와 server를 연결
+app.io.attach(server)
+server.listen(port, () => {
     console.log(`Server started at port ${port}`)
 })
